445bc2eeb3af54e5a6b2c5fe6a27d903090203f4
DesCritas <nik1234nik@gmail.com>
2022-12-30 18:36:19 +0300

    LikedById and LikeById functions now work using Async technology

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -2,12 +2,12 @@ package ru.netology.nmedia.repository
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.internal.EMPTY_REQUEST
 import ru.netology.nmedia.dto.Post
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 
@@ -51,36 +51,54 @@ class PostRepositoryImpl : PostRepository {
 //
     //}
 
-    override fun likeById(post: Post): Post {
+    override fun likeByIdAsync(post: Post, callback: PostRepository.AsyncCallback<Post>) {
         //val post = getById(id)
         val id = post.id
+
         if (!post.likedByMe) {
             val request: Request = Request.Builder()
                 .post(gson.toJson(EMPTY_REQUEST).toRequestBody(jsonType))
                 .url("${BASE_URL}/api/slow/posts/$id/likes")
                 .build()
-            return client.newCall(request)
-                .execute()
-                .let {
-                    it.body?.string() ?: throw RuntimeException("body is null")
-                }
-                .let {
-                    gson.fromJson(it, typeTokenPost.type)
-                }
+
+            client.newCall(request)
+                .enqueue(object : Callback {
+                    override fun onResponse(call: Call, response: Response) {
+                        val body = response.body?.string() ?: throw RuntimeException("Null body")
+                        try {
+                            callback.onSuccess(gson.fromJson(body, typeTokenPost.type))
+                        } catch (e: Exception) {
+                            callback.onError(e)
+                        }
+                    }
+
+                    override fun onFailure(call: Call, e: IOException) {
+                        callback.onError(e)
+                    }
+                })
         } else {
             val request: Request = Request.Builder()
                 .delete()
                 .url("${BASE_URL}/api/slow/posts/$id/likes")
                 .build()
-            return client.newCall(request)
-                .execute()
-                .let {
-                    it.body?.string() ?: throw RuntimeException("body is null")
-                }
-                .let {
-                    gson.fromJson(it, typeTokenPost.type)
-                }
+            client.newCall(request)
+                .enqueue(object : Callback {
+                    override fun onResponse(call: Call, response: Response) {
+                        val body = response.body?.string() ?: throw RuntimeException("Null body")
+                        try {
+                            callback.onSuccess(gson.fromJson(body, typeTokenPost.type))
+                        } catch (e: Exception) {
+                            callback.onError(e)
+                        }
+                    }
+
+                    override fun onFailure(call: Call, e: IOException) {
+                        callback.onError(e)
+                    }
+                })
+
         }
+
     }
 
     override fun save(post: Post) {
@@ -103,5 +121,28 @@ class PostRepositoryImpl : PostRepository {
         client.newCall(request)
             .execute()
             .close()
+    }
+
+    override fun getAllAsync(callback: PostRepository.AsyncCallback<List<Post>>) {
+        val request: Request = Request.Builder()
+            .url("${BASE_URL}/api/slow/posts")
+            .build()
+
+        client.newCall(request)
+            .enqueue(object : Callback {
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response.body?.string() ?: throw RuntimeException("Null body")
+
+                    try {
+                        callback.onSuccess(gson.fromJson(body, typeToken.type))
+                    } catch (e: Exception) {
+                        callback.onError(e)
+                    }
+
+                }
+                override fun onFailure(call: Call, e: IOException) {
+                    callback.onError(e)
+                }
+            })
     }
 }
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,10 +1,13 @@
 package ru.netology.nmedia.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.model.FeedModel
-import ru.netology.nmedia.repository.*
+import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryImpl
 import ru.netology.nmedia.util.SingleLiveEvent
 import java.io.IOException
 import kotlin.concurrent.thread
@@ -34,19 +37,22 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun loadPosts() {
-        thread {
-            // Начинаем загрузку
-            _data.postValue(FeedModel(loading = true))
-            try {
-                // Данные успешно получены
-                val posts = repository.getAll()
-                FeedModel(posts = posts, empty = posts.isEmpty())
-            } catch (e: IOException) {
-                // Получена ошибка
-                FeedModel(error = true)
-            }.also(_data::postValue)
-        }
+
+        // Начинаем загрузку
+        _data.value = FeedModel(loading = true)
+        // Данные успешно получены
+
+        repository.getAllAsync(object : PostRepository.AsyncCallback<List<Post>> {
+            override fun onSuccess(posts: List<Post>) {
+                _data.postValue(FeedModel(posts = posts, empty = posts.isEmpty()))
+            }
+
+            override fun onError(e: Exception) {
+                _data.postValue(FeedModel(error = true))
+            }
+        })
     }
+
 
     fun save() {
         edited.value?.let {
@@ -70,21 +76,24 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
         edited.value = edited.value?.copy(content = text)
     }
 
-    fun likeById(post : Post) {
-        thread {
+    fun likeById(post: Post) {
 
-            val likedPost: Post = repository.likeById(post)
 
-            _data.postValue(_data.value?.posts?.map {
-                if (it.id == likedPost.id) likedPost else it
-            }?.let {
-                _data.value?.copy(posts = it)
-            })
+        repository.likeByIdAsync(post, object : PostRepository.AsyncCallback<Post> {
+            override fun onSuccess(posts: Post) {
+                _data.postValue(_data.value?.posts?.map {
+                    if (it.id == posts.id) posts else it
+                }?.let {
+                    _data.value?.copy(posts = it)
+                })
+            }
 
-            //sleep(6000)
-            //loadPosts()
+            override fun onError(e: Exception) {
+                _data.postValue(FeedModel(error = true))
+            }
+        })
 
-        }
+
     }
 
     fun removeById(id: Long) {
